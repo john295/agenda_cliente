a26a6f384705fc7043f1e227ee0fa00095d9748d
noescobar <nelson.holic@gmail.com>
2015-12-14 19:21:16 -0500

    eliminacion de las citas concretadas

--- agenda/apps.py
+++ agenda/apps.py
@@ -1,5 +1,23 @@
 from django.apps import AppConfig
+import threading
+import time
+import datetime
 
+from agenda.models import Citas
+
+
+class CitaCheckThread(threading.Thread):
+
+    def run(self):
+        while True:
+            today = datetime.datetime.today()
+            citas = Citas.objects.filter(fecha_limite__lte=today)
+            citas.delete()
+            time.sleep(10)
 
 class AgendaConfig(AppConfig):
     name = 'agenda'
+
+    def ready(self):
+        cita_check_thread = CitaCheckThread()
+        cita_check_thread.start()
--- agenda_cliente/settings.py
+++ agenda_cliente/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'agenda'
+    'agenda.apps.AgendaConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
